df21da4be56b4f5378cdd63d6ad33956fd19f2d7
Nephe <guillaume.hetier@polytechnique.edu>
2015-09-28 17:26:07 +0200

    Second example

--- memoisation/src/main.cpp
+++ memoisation/src/main.cpp
@@ -8,42 +8,54 @@ using namespace std;
 struct Memoise {
     unordered_map<int, int> table;
     function<list<int>(int)> next;
-    function<int(int,int)> action;
-
-    Memoise (){
-        table = {{0,1},{1,1}};
-    }
+    function<int(list<int>, int)> action;
 
     Memoise (const unordered_map<int,int>& init,
-        const function<list<int>(int)> &next, const function<int(int,int)> &action)
+        const function<list<int>(int)> &next, const function<int(list<int>, int)> &action)
         :table(init){
             this->next = next;
             this->action = action;
     }
 
-    int fiboAuto (int n){
+    int fMem (int n){
         try{
             return table.at(n);;
         }
         catch(const out_of_range& e){
             list<int> needed = next(n);
-            int a = fiboAuto(needed.front());
-            needed.pop_front();
-            int b = fiboAuto(needed.front());
+            list<int> result;
+            for(int i: needed)
+                result.push_back(fMem(i));
 
-            return (table[n] = action(a,b));
+            return (table[n] = action(result, n));
         }
+    }
+
+    const unordered_map<int, int>& getTable() const {
+        return table;
     }
 
 };
 
 int main(){
-    Memoise m({{0,1},{1,1}},
+    Memoise fibo({{0,1},{1,1}},
         [](int n)->list<int>{ return {n-1, n-2};},
-        [](int a, int b)->int { return a+b;});
+        [](list<int> l, int n)->int { int r = 0; for(int i : l) r += i; return r;});
 
-    cout << "Hello world !" << endl;
-    cout << "Fibonacci 5 : " << m.fiboAuto(5) << endl;
+    cout << "Fibonacci 5 : " << fibo.fMem(5) << endl;
+
+    vector<int> tab({1, 2, 5, -4, 3});
+    Memoise subSum({{0,tab[0]}},
+        [](int n)->list<int>{ list<int> l; for(int i=0; i<n; l.push_back(i++)); return l;},
+        [tab](list<int> l, int n)->int{ return tab[n] >= 0 ? l.back() + tab[n] : tab[n];});
+
+    subSum.fMem(4);
+    int m = 0;
+    for(auto i : subSum.getTable()){
+        if(i.second > m)
+            m = i.second;
+    }
+    std::cout << "Plus grand sous tableau : " << m << std::endl;
 
     return 0;
 }
