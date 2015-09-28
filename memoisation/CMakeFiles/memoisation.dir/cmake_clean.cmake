a27684287337f13ae77c9485fba12e9c864d6fb3
Nephe <guillaume.hetier@polytechnique.edu>
2015-09-28 16:34:19 +0200

    Première tentative, non générique

---
+++ memoisation/CMakeFiles/memoisation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/memoisation.dir/src/main.cpp.o"
+  "memoisation.pdb"
+  "memoisation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/memoisation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- memoisation/src/main.cpp
+++ memoisation/src/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include <unordered_map>
+#include <list>
+#include <algorithm>
+
+using namespace std;
+
+struct Memoise {
+    unordered_map<int, int> table;
+    function<list<int>(int)> next;
+    function<int(int,int)> action;
+
+    Memoise (){
+        table = {{0,1},{1,1}};
+    }
+
+    Memoise (const unordered_map<int,int>& init,
+        const function<list<int>(int)> &next, const function<int(int,int)> &action)
+        :table(init){
+            this->next = next;
+            this->action = action;
+    }
+
+    int fiboAuto (int n){
+        try{
+            return table.at(n);;
+        }
+        catch(const out_of_range& e){
+            list<int> needed = next(n);
+            int a = fiboAuto(needed.front());
+            needed.pop_front();
+            int b = fiboAuto(needed.front());
+
+            return (table[n] = action(a,b));
+        }
+    }
+
+};
 
 int main(){
-    std::cout << "Hello world !" << std::endl;
+    Memoise m({{0,1},{1,1}},
+        [](int n)->list<int>{ return {n-1, n-2};},
+        [](int a, int b)->int { return a+b;});
+
+    cout << "Hello world !" << endl;
+    cout << "Fibonacci 5 : " << m.fiboAuto(5) << endl;
 
     return 0;
 }
