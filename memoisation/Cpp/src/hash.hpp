0ec22c82b91430674a11fd5ff029a5722538dc79
Nephe <guillaume.hetier@polytechnique.edu>
2015-10-20 22:15:55 +0200

    hash for vectors, pairs and arrays

--- memoisation/Cpp/src/hash.hpp
+++ memoisation/Cpp/src/hash.hpp
@@ -2,6 +2,8 @@
 #define HASH_HPP
 
 #include <functional>
+#include <vector>
+#include <array>
 
 namespace mem{
 
@@ -28,26 +30,55 @@ private:
     bool isFunctionDef;
 };
 
+namespace{
+    // Combine the hash value of v to another hashvalue seed
+    template <class T>
+    inline void hash_combine(std::size_t& seed, T const& v)
+    {
+        // Come from boost::hash_combine, use golden number to spread entropy
+        seed ^= mem::hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+    }
+}
+
 // Specilization for std::pair
 template <typename U, typename V>
 class hash<std::pair<U, V>>{
 public:
     size_t operator()(std::pair<U,V> const& tt) const {
-        return ((std::hash<U>()(std::get<0>(tt))
-               ^ (std::hash<V>()(std::get<1>(tt)) << 1)) >> 1);
+        size_t seed = 0;
+        hash_combine(seed, std::get<0>(tt));
+        hash_combine(seed, std::get<1>(tt));
+        return seed;
+    }
+};
+
+// Specilization for std::vector
+template <typename T>
+class hash<std::vector<T>>{
+public:
+    size_t operator()(std::vector<T> const& tt) const {
+        size_t seed = 0;
+        for(auto i = tt.begin(); i != tt.end(); i++){
+            hash_combine(seed, *i);
+        }
+        return seed;
+    }
+};
+
+// Specilization for std::array
+template <typename T, int N>
+class hash<std::array<T, N>>{
+public:
+    size_t operator()(std::array<T,N> const& tt) const {
+        size_t seed = 0;
+        for(auto i = tt.begin(); i != tt.end(); i++){
+            hash_combine(seed, *i);
+        }
+        return seed;
     }
 };
 
 namespace {
-
-    // Combine the hash value of v to another hashvalue in seed
-    template <class T>
-    inline void hash_combine(std::size_t& seed, T const& v)
-    {
-        // Come from boost::hash_combine
-        seed ^= mem::hash<T>()(v) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-    }
-
     // Recursive template code derived from Matthieu M.
     template <class Tuple, size_t Index = std::tuple_size<Tuple>::value - 1>
     struct HashValueImpl
@@ -79,10 +110,6 @@ public:
         return seed;
     }
 };
-
-// Specialisation for std::vector
-//TODO
-
 
 }
 #endif
